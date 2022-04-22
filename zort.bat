bf9a5d773aeb6e127955a5dab8b7a3c7969c2bee
Arisune <98884341+Arisunecik@users.noreply.github.com>
2022-04-22 22:45:06 +0300

    Update zort.bat

--- zort.bat
+++ zort.bat
@@ -1,2 +1,3 @@
 @echo off
 "C:\Users\%username%\AppData\Roaming\.craftrise\java\jdk-x64\bin\java.exe" -Xmx3840M -Djava.net.preferIPv4Stack=true -XX:+DisableAttachMechanism -Dcom.ibm.tools.attach.enable=no -Xmn128M -jar "C:\Users\%username%\AppData\Roaming\.craftrise\launcher.jar" launcherStartup
+pause
